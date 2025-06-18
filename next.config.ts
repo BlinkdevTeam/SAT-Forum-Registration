475ac03104e4fc506ce60f4cffd2678060993b2a
Lander Guevarra <landerguevarra.blink@gmail.com>
2025-06-18 15:25:38 +0800

    remove static2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/Species-Advancement-Tech-Forum" : "",
-  assetPrefix: isProd ? "/Species-Advancement-Tech-Forum/" : "",
-  images: {
-    unoptimized: true, // ← Add this
-  },
+  /* config options here */
 };
 
 export default nextConfig;
