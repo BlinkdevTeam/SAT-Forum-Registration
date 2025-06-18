d9a1ceb119d947ecfe723190a2c5686dcd27e052
Lander Guevarra <landerguevarra.blink@gmail.com>
2025-06-18 13:21:30 +0800

    out folder created

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/new_website" : "",
+  assetPrefix: isProd ? "/new_website/" : "",
 };
 
 export default nextConfig;
